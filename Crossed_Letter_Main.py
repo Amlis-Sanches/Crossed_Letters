c05da78c652863a7ff369809a1d398448fc26b4b
Amlis-Sanches <nathanielchorn@nchsande.com>
2023-12-07 21:30:54 -0600

    description and clean text function

--- Crossed_Letter_Main.py
+++ Crossed_Letter_Main.py
@@ -1,3 +1,31 @@
+'''
+Crossed Letter Image Generator v0.0.1
+Last edit made by: Nathaniel Horn
+#------------------------------------------------------------------------------#
+This Python script is designed to transform text from various file formats 
+(text, Word, PDF) into a 'Crossed Letter' image or document. The process involves:
+
+Inputs:
+- File path of the text source.
+- File type (text, word, pdf).
+
+Steps:
+1. Extract text from the input file.
+2. Clean the extracted text for processing.
+3. Generate an image or document where:
+   a. The first set of text runs left to right in blue.
+   b. The document/image is then rotated 90 degrees.
+   c. A second set of text is overlaid in red.
+   d. Where texts overlap, the colors blend to create purple.
+4. Save the output as an image or document.
+
+Desired Output:
+A Crossed Letter image or document with the specified text, saved in the user's system.
+The image will have the first set of text running left to right in blue, and the second
+set of text running top to bottom in red. Where the texts overlap, the colors will blend
+to create purple.
+'''
+
 # Import necessary libraries
 import docx
 import PyPDF2
@@ -35,10 +63,12 @@ def extract_text(file_path, file_type):
 def text_clean(text):
     #clean text for generation
     cleaned_text = text.replace('\n', '')
+    cleaned_text = cleaned_text.replace('\t', '')
+    #shape test to fit for the desired image
+    for i in range(0, len(cleaned_text), 100):
+        if i % 100 == 0:
+            cleaned_text = cleaned_text[:i] + '\n' + cleaned_text[i:]
     return cleaned_text
-
-    pass
-    
 
 # Function to generate crossed letter image/document
 def generate_crossed_letter(text):
